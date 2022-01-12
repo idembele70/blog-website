16e3a49cc7235d9e9d30977b7a4debe5b94ded9f
idembele70 <idembele70@gmail.com>
2022-01-12 13:02:25 +0100

    [index.js] fixing multer filename

--- index.js
+++ index.js
@@ -1,51 +1,53 @@
-const express=require("express");
-const app=express();
-const dotenv=require("dotenv");
-const mongoose=require("mongoose")
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose")
 dotenv.config();
-const authRouth=require("./routes/auth");
+const authRouth = require("./routes/auth");
 app.use(express.json());
 const cors = require('cors')
 app.use(cors())
-const multer=require("multer");
-const userRouth=require("./routes/users");
-const postRouth=require("./routes/posts");
-const categoryRouth=require("./routes/categories");
+const multer = require("multer");
+const userRouth = require("./routes/users");
+const postRouth = require("./routes/posts");
+const categoryRouth = require("./routes/categories");
 
-const path=require("path");
-app.use("/images",express.static(path.join(__dirname,"/images")));
+const path = require("path");
+const { resolve } = require("path");
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
 
-mongoose.connect(process.env.DATABASE,{
+mongoose.connect(process.env.DATABASE, {
     // useFindAndModify: true, 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
     // useCreateIndex:true
 }).then(console.log("db connected"))
-.catch((err)=>console.log(err));
+    .catch((err) => console.log(err));
 
 
-const storage=multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,"images");
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, resolve(__dirname, "images"));
     },
-    filename:(req,file,cb)=>{
-        cb(null,req.body.name);
+    filename: (req, file, cb) => {
+        console.log(file)
+        cb(null, Date.now() + file.originalname);
     },
 });
-const upload=multer({storage:storage});
-app.post("/api/upload",upload.single("file"),(req,res)=>{
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
     res.status(200).json("file has been uploaded");
 });
 
 
-app.use("/api/users",userRouth);
-app.use("/api/auth",authRouth);
-app.use("/api/posts",postRouth);
-app.use("/api/categories",categoryRouth);
+app.use("/api/users", userRouth);
+app.use("/api/auth", authRouth);
+app.use("/api/posts", postRouth);
+app.use("/api/categories", categoryRouth);
 
 
-app.use("/pp",(req,res)=>{
+app.use("/pp", (req, res) => {
     console.log("hey this is pp url")
     res.send("from pp")
 });
@@ -55,7 +57,7 @@ app.use("/pp",(req,res)=>{
 //     res.send("hiiiiii")
 // });
 
-app.listen("8000",()=>{
+app.listen("8000", () => {
     console.log("listerning to port ")
 });
 
